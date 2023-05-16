851ae3a485e6625c4e4e856d0dfaccfa99b80f0f
Nsikak Thomas <nsikakthomas102@gmail.com>
2023-05-16 13:43:05 +0100

    Layout created & used to wrap the _app component

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,11 @@
+import { useEffect, useState } from 'react'
 import '@/styles/globals.sass'
+import { Layout } from '@/components'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }

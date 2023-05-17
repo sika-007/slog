50dfe7d589f67d91cec106cb4488108d6590032c
Nsikak Thomas <nsikakthomas102@gmail.com>
2023-05-17 12:00:52 +0100

    Header component placed in application layout

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,19 +2,30 @@ import { useContext } from "react"
 import Link from "next/link"
 
 
+const categories = [
+	{ name: "react", slug: "react" },
+	{ name: "Web Development", slug: "web-dev" }
+]
+
 const Header = () => {
 	return (
 		<div className="container mx-auto px-10 mb-8">
 			<div className="border-b w-full	inline-block border-blue-400 py-8">
 				<div className="md:float-left block">
 					<Link href="/">
-						<span className="cursor-pointer font-bold text-4xl text-white">
-							Slog!
-						</span>
+						<p className="cursor-pointer font-bold text-4xl text-white">
+							THE SLOG!
+						</p>
 					</Link>
 				</div>
 				<div className="hidden md:float-left md:contents">
-					
+					{categories.map(category => (
+						<Link key={category.slug} href={`/category/${category.slug}`}>
+							<p className="md:float-right mt-2 align-middle text-white ml-4 font-semibold">
+								{category.name}
+							</p>
+						</Link>
+					))}
 				</div>
 			</div>
 		</div>

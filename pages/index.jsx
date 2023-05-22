f1a2ff30ae5d1789a2a9633659ddc163e1325009
Nsikak Thomas <nsikakthomas102@gmail.com>
2023-05-22 19:03:53 +0100

    dynamic props imported using graphql

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,15 +1,28 @@
 import { Inter } from 'next/font/google'
 import Head from 'next/head'
 import { PostCard, Categories, PostWidget } from '@/components'
+import { getPosts, getRecentPosts } from '@/services'
 
 const inter = Inter({ subsets: ['latin'] })
 
-const posts = [
-  { title: "React Testing", excerpt: "Learn React Testing" },
-  { title: "React with Tailwind", excerpt: "Learn React With tailwind css" },
-]
+export async function getStaticProps() {
+  const posts = await getPosts()
 
-export default function Home() {
+  return (
+    {
+      props: {
+        posts
+      }
+    }
+  )
+}
+
+
+
+export default function Home({ posts }) {
+
+  console.log(posts)
+
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
@@ -18,10 +31,10 @@ export default function Home() {
       </Head>
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className='lg:col-span-8 col-span-1'>
-          {posts.map(post => <PostCard post={post} key={post.title}/>)}
+          {posts.map(post => <PostCard post={post.node} key={post.node.title} />)}
         </div>
         <div className='lg:col-span-4 col-span-1'>
-          <div className='lg:sticly relative top-8'>
+          <div className='lg:sticky relative top-8'>
             <PostWidget />
             <Categories />
           </div>

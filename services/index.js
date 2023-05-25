e963dc227f21eaba6b48af3a85c45b32171e554c
Nsikak Thomas <nsikakthomas102@gmail.com>
2023-05-25 23:07:18 +0100

    NextJS POST API call funcition added to services

--- services/index.js
+++ services/index.js
@@ -120,3 +120,15 @@ export const getPostDetails = async (slug) => {
 	const result = await request(graphqlAPI, query, { slug })
 	return result.post
 }
+
+export const submitComment = async (obj) => {
+	const result = await fetch("/api/comments", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json"
+		},
+		body: JSON.stringify(obj)
+	})
+
+	return result.json()
+}

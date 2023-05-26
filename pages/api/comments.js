64e2ecf3992082732c2eed0914a8fdf39c10f1c8
Nsikak Thomas <nsikakthomas102@gmail.com>
2023-05-26 18:38:00 +0100

    backend refactored with try-catch

--- pages/api/comments.js
+++ pages/api/comments.js
@@ -7,33 +7,38 @@ const hygraphToken = process.env.GRAPHCMS_TOKEN
 
 export default async function comments(req, res) {
 
-  const { name, email, comment, slug } = req.body
-
-
-  const graphQLClient = new GraphQLClient(graphqlAPI, {
-    headers: {
-      authorization: `Bearer ${hygraphToken}`,
-    }
-  })
-
-  console.log("")
-
-  const query = gql`
-    mutation CreateComment($name: String!, $email: String!, $comment: String!, $slug: String!) {
-      createComment(
-        data: {name: $name, email: $email, comment: $comment, cli34hbl915ef01ta1uybfy7w: {connect: {slug: $slug}}} 
-      ) { id }
-    }
-  `
-
-
-  const result = await graphQLClient.request(query, {
-    name,
-    email,
-    comment,
-    slug
-  })
-
-  res.status(200).send(result)
-
+  if (req.method === "POST") {
+    const { name, email, comment, slug } = req.body
+  
+  
+    const graphQLClient = new GraphQLClient(graphqlAPI, {
+      headers: {
+        authorization: `Bearer ${hygraphToken}`,
+      }
+    })
+  
+    console.log("")
+  
+    const query = gql`
+      mutation CreateComment($name: String!, $email: String!, $comment: String!, $slug: String!) {
+        createComment(
+          data: {name: $name, email: $email, comment: $comment, post: {connect: {slug: $slug}}} 
+        ) { id }
+      }
+    `
+  
+  
+      try {
+        const result = await graphQLClient.request(query, {
+          name,
+          email,
+          comment,
+          slug
+        })
+      
+        res.status(200).json(result)
+      } catch (error) {
+        res.status(500).json(error)
+      }
+  }
 }

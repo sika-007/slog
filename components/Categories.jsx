b1c43348e603548604701bba1ac9a710e38afedd
Nsikak Thomas <nsikakthomas102@gmail.com>
2023-05-29 08:08:10 +0100

    padding optimaizations

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -16,14 +16,14 @@ const Categories = () => {
 
   const categoryItems = categories?.map(category => (
     <Link key={category.slug} href={`/categories/${category.slug}`}>
-      <p className="cursor-pointer block pb-3 mb-3">
+      <p className="cursor-pointer block pb-3 ">
         {category.name}
       </p>
     </Link>
   ))
 
   return (
-    <div className="bg-white shadow-lg rounded-lg p-8 mb-8">
+    <div className="bg-white shadow-lg rounded-lg p-8 mb-8 lg:overflow-auto">
       <h3 className='text-xl mb-8 font-semibold border-b pb-4'>
         Categories
       </h3>
